23e74077241c512d3a791fa5c7b4c1ea703a67a7
Jarosław Zwierz <j20011812@o2.pl>
2023-02-27 21:13:18 +0100

    nav finished for now (need work on animation)

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './nav.scss'
 import hamburger from '../../assets/hamburger-light.png'
 
 function Nav() {
+  const [isNavExpanded, setIsNavExpanded] = useState(false)
   return (
     <div className='nav-wrapper'>
       <div className='nav-logo'>
         Portfolio
       </div>
-      <div className='nav-hamburger'>
-        <img src={hamburger} alt='hamburger menu' id='nav-hamburger'/>
-      </div>
-      <div className='nav-list-wrapper'>
+      <button className='nav-button' onClick={() => {setIsNavExpanded(!isNavExpanded);}}>
+        <img src={hamburger} alt='menu button' id='nav-button-img'/>
+      </button>
+      <div className={
+          isNavExpanded ? "nav-list expanded" : "nav-list"
+        }>
         <li><a href='#home'>Home</a></li>
         <li><a href='#about'>About</a></li>
         <li><a href='#skills'>Skills</a></li>
