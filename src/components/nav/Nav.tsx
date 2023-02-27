d8a9d0cbcde29547f8e66005633e6a1452a1e133
Jarosław Zwierz <j20011812@o2.pl>
2023-02-27 20:23:34 +0100

    some nav work

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './nav.scss'
+import hamburger from '../../assets/hamburger-light.png'
 
 function Nav() {
   return (
     <div className='nav-wrapper'>
-      Nav
+      <div className='nav-logo'>
+        Portfolio
+      </div>
+      <div className='nav-hamburger'>
+        <img src={hamburger} alt='hamburger menu' id='nav-hamburger'/>
+      </div>
+      <div className='nav-list-wrapper'>
+        <li><a href='#home'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#skills'>Skills</a></li>
+        <li><a href='#projects'>Projects</a></li>
+      </div>
     </div>
   )
 }
