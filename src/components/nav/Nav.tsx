8dc4e76fb9badce826c52b608bc3c67458c35bd8
Jarosław Zwierz <j20011812@o2.pl>
2023-03-06 15:36:20 +0100

    work on skills panel, some css fixes (still need responsivene)

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -25,11 +25,11 @@ function Nav() {
           <div className={
             isNavExpanded ? "nav-list expanded" : "nav-list"
           }>
-          <li className='nav-item'><a href='#home'>Home</a></li>
-          <li><a href='#about'>About</a></li>
-          <li><a href='#skills'>Skills</a></li>
-          <li><a href='#projects'>Projects</a></li>
-        </div>
+            <li className='nav-item'><a href='#home'>Home</a></li>
+            <li><a href='#about'>About</a></li>
+            <li><a href='#skills'>Skills</a></li>
+            <li><a href='#projects'>Projects</a></li>
+          </div>
         </>
       )
     }
